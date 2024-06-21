d7b064bba356f51a3ab9d46dbe5ac4a651eb3fb2
Cory W. Cordell <cory.cordell@myemail.com>
2024-06-21 12:38:47 -0500

    reverted classes

--- api/Models/Leaderboard.cs
+++ api/Models/Leaderboard.cs
@@ -7,22 +7,9 @@ namespace Api.Models
         public required string Handle { get; init; }
 
         // The score achieved by the player
-        private int _score;
-
-        public int Score
-        {
-            get => _score;
-            init
-            {
-                if (value < 0)
-                {
-                    throw new ArgumentException("Score must be greater than or equal to 0.");
-                }
-                _score = value;
-            }
-        }
+        public int Score { get; init; }
 
         // The date and time when the game was completed
-        public required DateTime DateTimePlayed { get; init; }
+        public DateTime DateTimePlayed { get; init; }
     }
 }
